2606cd4bb6ff9677bf8e19626f69cd36d6526cb4
Urvish Mali <113707301+KrishnaCodez@users.noreply.github.com>
2024-11-02 09:14:45 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://magictext.tech",
+  siteUrl: "https://magictext.tech/",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
