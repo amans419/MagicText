16a5f7fdac6fd8bc1f4143ed058b39a30419ca0b
Aman Singh <130776837+amans419@users.noreply.github.com>
2025-01-29 19:07:16 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://magictext.tech/",
+  siteUrl: "https://textbehindimages.vercel.app/",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
